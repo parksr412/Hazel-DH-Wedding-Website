07b3d7ed1e915431d5e1c6ce6d4fc651dd4988a0
Rebecca Parks <parksr412@gmail.com>
2023-03-13 17:35:53 -0400

    Update random image feature

--- Hazel-DH-Wedding.js
+++ Hazel-DH-Wedding.js
@@ -1,15 +1,12 @@
 
-let arrayOfImages = [];
+let arrayOfImages = [
+    "https://i.imgur.com/7QtU8VF.jpg",
+    "https://i.imgur.com/xolg4JT.jpg",
+    "https://i.imgur.com/9sSwibn.jpg",
+    "https://i.imgur.com/tmQIivs.jpg",
+    "https://i.imgur.com/Ps8QY2L.jpg",
+];
 
-arrayOfImages.push("https://i.imgur.com/7QtU8VF.jpg");
-arrayOfImages.push("https://i.imgur.com/xolg4JT.jpg");
-arrayOfImages.push("https://i.imgur.com/9sSwibn.jpg");
-arrayOfImages.push("https://i.imgur.com/tmQIivs.jpg");
-arrayOfImages.push("https://i.imgur.com/Ps8QY2L.jpg");
 
-let imagesLength = arrayOfImages.length;
-let randomNumber = Math.random();
-randomNumber = randomNumber*imagesLength;
-randomNumber = Math.floor(randomNumber);
-let chosenImage = arrayOfImages[randomNumber];
-featured.src = chosenImage;
+const randomImageIndex = Math.floor(Math.random() * arrayOfImages.length);
+featured.src = arrayOfImages[randomImageIndex];
