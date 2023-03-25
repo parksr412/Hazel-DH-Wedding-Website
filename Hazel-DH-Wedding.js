affdb5175d1d8264bcb556111b5c23b31970e861
Rebecca Parks <parksr412@gmail.com>
2023-03-25 19:17:53 -0400

    Update layout

--- Hazel-DH-Wedding.js
+++ Hazel-DH-Wedding.js
@@ -7,6 +7,15 @@ let arrayOfImages = [
     "https://i.imgur.com/Ps8QY2L.jpg",
 ];
 
+window.addEventListener("load", function() {
+    document.querySelector(".no-flash-of-unstyled-content").classList.remove("no-flash-of-unstyled-content");
+
+    // get the thing
+
+    // add the class to the thing
+});
+
+
 
 const randomImageIndex = Math.floor(Math.random() * arrayOfImages.length);
 featured.src = arrayOfImages[randomImageIndex];
